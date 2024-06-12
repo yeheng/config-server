13b9405ff8893e6987822ceb5e97e1f5bd78f4ce
Heng.Ye <hengheng8848@gmail.com>
2024-06-12 13:59:26 +0800

    fix sonarcloud config

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -7,6 +7,6 @@ pub mod release;
 pub mod namespace;
 pub mod security;
 
-pub const OP_INSERT: &'static str = "INSERT";
-pub const OP_UPDATE: &'static str = "UPDATE";
-pub const OP_DELETE: &'static str = "DELETE";
+pub const OP_INSERT: &str = "INSERT";
+pub const OP_UPDATE: &str = "UPDATE";
+pub const OP_DELETE: &str = "DELETE";
