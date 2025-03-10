a5644a074472dbab35101a2a30d86042db46b927
Heng.Ye <hengheng8848@gmail.com>
2025-03-10 19:11:49 +0800

    refactor(database): 更新数据库连接 URL 并重构实体
    - 更新 .env 文件中的 DATABASE_URL 为 PostgreSQL 数据库
    - 删除多个未使用的实体文件
    - 新增多个实体文件，包括 access_key、app、app_namespace 等
    - 更新 Cargo.toml 文件，添加新的依赖项
    - 重命名 sys_follow_log 表为 instance

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,1 @@
-use sea_orm::{Database, DbErr};
-
-const DATABASE_URL: &str = "mysql://root:password@localhost:3306";
-const DB_NAME: &str = "bakeries_db";
-
-async fn run() -> Result<(), DbErr> {
-    let _db = Database::connect(DATABASE_URL).await?;
-
-    Ok(())
-}
-
-fn main() {
-}
+fn main() {}
